129a42f28f539178314379b9e7809ee2858adeca
Javier <javier.manzano22@gmail.com>
2018-09-28 16:19:04 +0200

    cryptos fixed. Real workin 100%

--- routes/cryptos.js
+++ routes/cryptos.js
@@ -17,21 +17,21 @@ router.get('/', (req, res, next) => {
     }
     const ETH = response.data.data[1027];
     if (Math.random() > 0.5) {
-      response.data.data[1027].quotes.USD.price = response.data.data[1].quotes.USD.price + deviation;
+      response.data.data[1027].quotes.USD.price = response.data.data[1027].quotes.USD.price + deviation;
     } else {
-      response.data.data[1027].quotes.USD.price = response.data.data[1].quotes.USD.price - deviation;
+      response.data.data[1027].quotes.USD.price = response.data.data[1027].quotes.USD.price - deviation;
     }
     const LTC = response.data.data[2];
     if (Math.random() > 0.5) {
-      response.data.data[2].quotes.USD.price = response.data.data[1].quotes.USD.price + deviation;
+      response.data.data[2].quotes.USD.price = response.data.data[2].quotes.USD.price + deviation;
     } else {
-      response.data.data[2].quotes.USD.price = response.data.data[1].quotes.USD.price - deviation;
+      response.data.data[2].quotes.USD.price = response.data.data[2].quotes.USD.price - deviation;
     }
     const TRX = response.data.data[1958];
     if (Math.random() > 0.5) {
-      response.data.data[1958].quotes.USD.price = response.data.data[1].quotes.USD.price + deviation;
+      response.data.data[1958].quotes.USD.price = response.data.data[1958].quotes.USD.price + deviation;
     } else {
-      response.data.data[1958].quotes.USD.price = response.data.data[1].quotes.USD.price - deviation;
+      response.data.data[1958].quotes.USD.price = response.data.data[1958].quotes.USD.price - deviation;
     }
     res.json([
       {
