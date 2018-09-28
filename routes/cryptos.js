96e03cd0d8d3acc8fe3addf0c337b4f97cec87ad
Javier <javier.manzano22@gmail.com>
2018-09-28 16:09:28 +0200

    math random

--- routes/cryptos.js
+++ routes/cryptos.js
@@ -8,10 +8,31 @@ router.get('/', (req, res, next) => {
     url: 'https://api.coinmarketcap.com/v2/ticker/?convert=USD&limit=100'
   }).then(response => {
     console.log(response.data.data);
+    const deviation = Math.random() /1000;
     const BTC = response.data.data[1];
+    if (Math.random() > 0.5) {
+      response.data.data[1].quotes.USD.price = response.data.data[1].quotes.USD.price + deviation;
+    } else {
+      response.data.data[1].quotes.USD.price = response.data.data[1].quotes.USD.price - deviation;
+    }
     const ETH = response.data.data[1027];
+    if (Math.random() > 0.5) {
+      response.data.data[1027].quotes.USD.price = response.data.data[1].quotes.USD.price + deviation;
+    } else {
+      response.data.data[1027].quotes.USD.price = response.data.data[1].quotes.USD.price - deviation;
+    }
     const LTC = response.data.data[2];
+    if (Math.random() > 0.5) {
+      response.data.data[2].quotes.USD.price = response.data.data[1].quotes.USD.price + deviation;
+    } else {
+      response.data.data[2].quotes.USD.price = response.data.data[1].quotes.USD.price - deviation;
+    }
     const TRX = response.data.data[1958];
+    if (Math.random() > 0.5) {
+      response.data.data[1958].quotes.USD.price = response.data.data[1].quotes.USD.price + deviation;
+    } else {
+      response.data.data[1958].quotes.USD.price = response.data.data[1].quotes.USD.price - deviation;
+    }
     res.json([
       {
         asset_id_name: 'BTC',
