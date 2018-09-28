1ec6fdfdc0ae613344140dd8e85521204d43ff55
Javier <javier.manzano22@gmail.com>
2018-09-28 15:45:18 +0200

    coins working

--- routes/cryptos.js
+++ routes/cryptos.js
@@ -3,34 +3,34 @@ var router = express.Router();
 const axios = require('axios');
 
 router.get('/', (req, res, next) => {
-  const BTCRequest = axios({
+  axios({
     method: 'get',
-    url: 'https://rest.coinapi.io/v1/exchangerate/BTC/USD',
-    headers: {'X-CoinAPI-Key': '8910FE5C-E094-41E5-83A3-18AEBBDF5E46'}
-  });
-
-  const ETHRequest = axios({
-    method: 'get',
-    url: 'https://rest.coinapi.io/v1/exchangerate/ETH/USD',
-    headers: {'X-CoinAPI-Key': '8910FE5C-E094-41E5-83A3-18AEBBDF5E46'}
-  });
-
-  const LTCRequest = axios({
-    method: 'get',
-    url: 'https://rest.coinapi.io/v1/exchangerate/LTC/USD',
-    headers: {'X-CoinAPI-Key': '8910FE5C-E094-41E5-83A3-18AEBBDF5E46'}
-  });
-
-  const TRXRequest = axios({
-    method: 'get',
-    url: 'https://rest.coinapi.io/v1/exchangerate/TRX/USD',
-    headers: {'X-CoinAPI-Key': '8910FE5C-E094-41E5-83A3-18AEBBDF5E46'}
-  });
-
-  Promise.all([BTCRequest,ETHRequest,LTCRequest,TRXRequest]).then(values => {
-    const cryptos = values.map(v => v.data);
-    res.json(cryptos);
-  });
+    url: 'https://api.coinmarketcap.com/v2/ticker/?convert=USD&limit=100'
+  }).then(response => {
+    console.log(response.data.data);
+    const BTC = response.data.data[1];
+    const ETH = response.data.data[1027];
+    const LTC = response.data.data[2];
+    const TRX = response.data.data[1958];
+    res.json([
+      {
+        asset_id_name: 'BTC',
+        rate: BTC.quotes.USD.price,
+      },
+      {
+        asset_id_name: 'ETH',
+        rate: ETH.quotes.USD.price,
+      },
+      {
+        asset_id_name: 'TRX',
+        rate: TRX.quotes.USD.price,
+      },
+      {
+        asset_id_name: 'LTC',
+        rate: LTC.quotes.USD.price,
+      },
+    ]);
+  })
 });
 
 module.exports = router;
