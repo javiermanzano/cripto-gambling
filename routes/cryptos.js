dacf1bcffa4290551a1ba1d6b695f80ab6780fe5
aPanfaloneDev <juegosfurno@gmail.com>
2018-09-28 10:38:25 +0100

    GET to crypto api

--- routes/cryptos.js
+++ routes/cryptos.js
@@ -1,11 +1,34 @@
 var express = require('express');
 var router = express.Router();
+const axios = require('axios');
 
 router.get('/', (req, res, next) => {
-  res.json({
-    ethereum: 0.002,
-    litecoin: 0.001,
-    bitcoin: -0.001,
+  const BTCRequest = axios({
+    method: 'get',
+    url: 'https://rest.coinapi.io/v1/exchangerate/BTC/USD',
+    headers: {'X-CoinAPI-Key': '8910FE5C-E094-41E5-83A3-18AEBBDF5E46'}
+  });
+
+  const ETHRequest = axios({
+    method: 'get',
+    url: 'https://rest.coinapi.io/v1/exchangerate/ETH/USD',
+    headers: {'X-CoinAPI-Key': '8910FE5C-E094-41E5-83A3-18AEBBDF5E46'}
+  });
+
+  const LTCRequest = axios({
+    method: 'get',
+    url: 'https://rest.coinapi.io/v1/exchangerate/LTC/USD',
+    headers: {'X-CoinAPI-Key': '8910FE5C-E094-41E5-83A3-18AEBBDF5E46'}
+  });
+
+  const TRXRequest = axios({
+    method: 'get',
+    url: 'https://rest.coinapi.io/v1/exchangerate/TRX/USD',
+    headers: {'X-CoinAPI-Key': '8910FE5C-E094-41E5-83A3-18AEBBDF5E46'}
+  });
+
+  Promise.all([BTCRequest,ETHRequest,LTCRequest,TRXRequest]).then( values => {
+    console.log(values.map(v => v.data));
   });
 });
 
